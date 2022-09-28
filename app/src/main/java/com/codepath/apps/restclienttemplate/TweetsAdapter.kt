dbf174c1c7299a84faa923a18b982c9153dc9617
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2022-09-28 17:59:08 -0400

    Added all of the tweet information and models

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -0,0 +1,45 @@
+package com.codepath.apps.restclienttemplate
+
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.codepath.apps.restclienttemplate.models.Tweet
+import java.util.zip.Inflater
+
+class TweetsAdapter(val tweets: List<Tweet>): RecyclerView.Adapter<TweetsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        // Inflate item layout
+        val view = inflater.inflate(R.layout.item_tweet, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get data model based on position
+        val tweet: Tweet = tweets.get(position)
+
+        // Set item views based on views and data model
+        holder.tvUsername.text = tweet.user?.name
+        holder.tvTweetBody.text = tweet.body
+
+        Glide.with(holder.itemView).load(tweet.user?.publicImageUrl).into(holder.ivProfileImage)
+    }
+
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
+        val tvUsername = itemView.findViewById<TextView>(R.id.tvUsername)
+        val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -0,0 +1,31 @@
+package com.codepath.apps.restclienttemplate.models
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+class Tweet {
+    var body: String = ""
+    var createdAt: String = ""
+    var user: User? = null
+
+    companion object {
+        // Create one tweet
+        fun fromJson(jsonObject: JSONObject): Tweet {
+            val tweet = Tweet()
+            tweet.body = jsonObject.getString("text")
+            tweet.createdAt = jsonObject.getString("created_at")
+            tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            return tweet
+        }
+
+        // Take a list of tweets
+        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
+            val tweets = ArrayList<Tweet>()
+            // until is exclusive - .. is inclusive
+            for (i in 0 until jsonArray.length()) {
+                tweets.add(fromJson(jsonArray.getJSONObject(i)))
+            }
+            return tweets
+        }
+    }
+}
